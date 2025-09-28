53868d6d6094af9d6a2c6206e0c45794e2c280bb
Naveen240720 <naveen63939@gmail.com>
2025-09-28 11:19:43 +0530

    Finalizing JWT implementation after renaming branch

--- auth-service/src/main/java/com/cinevision/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/cinevision/auth_service/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.cinevision.auth_service.controller;
 
+import com.cinevision.auth_service.dto.AuthResponse;
+import com.cinevision.auth_service.dto.LoginRequest;
 import com.cinevision.auth_service.dto.UserDTO;
 import com.cinevision.auth_service.model.User;
+import com.cinevision.auth_service.service.AuthService;
 import com.cinevision.auth_service.service.UserService;
 import org.springframework.http.ResponseEntity;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +16,18 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor  // ✅ generates constructor for final fields
 public class AuthController {
 
-    private final UserService userService; // ✅ no @Autowired needed
+    private final UserService userService;
+    private final AuthService authService;// ✅ no @Autowired needed
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody UserDTO userDTO) {
         User user = userService.registerUser(userDTO);
         return ResponseEntity.ok(user);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        AuthResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/user/{username}")
